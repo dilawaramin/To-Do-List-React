7343fe16e51a2c3e7fd23868880b989d345872b5
Dilawar Amin <79779873+dilawaramin@users.noreply.github.com>
2024-04-28 19:39:38 -0400

    task styling

--- client/src/Task.jsx
+++ client/src/Task.jsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import trash from './icons/trash-can.png'
 
 function Task( {taskName, onDelete} ) {
 
@@ -8,19 +9,21 @@ function Task( {taskName, onDelete} ) {
 
         <div className='task'>
             <div className='task-check'>
-                <button 
-                    className='checkbox'>
-                        
+                <button className='checkbox'>
+                    <img src=''/>
                 </button>
+                
             </div>
             <div className='task-description'>
                 <h3>{taskName}</h3>
             </div>
             <div className='delete-button-div'>
                 <button 
-                    onClick={() => onDelete(taskName.id)}
-                    className='delete-button'>
-                    Delete
+                    className='delete-button'
+                    onClick={() => onDelete(taskName.id)}>
+                        <img 
+                            className="trash-image"
+                            src={trash}/>
                 </button>
             </div>
         </div>
